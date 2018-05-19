ef9aa7490a962854eb4fde343a7d1431fa85ddae
Ozair Khan <ozairsaddress@gmail.com>
2018-05-19 12:28:52 -0600

    fixed bug on logic.js and api routes

--- public/js/members.js
+++ public/js/members.js
@@ -1,16 +1,16 @@
 // get grocery items from databse, and sorts them in shlpping list or pantry
-$.get("/members/getgroceries2").then(function (data) {
+$.get("/api/getgroceries").then(function (data) {
   for (var i = 0; i < data.length; i++) {
-    if(!data[i].ownedItem){
+    if (!data[i].ownedItem) {
       $("#groceriesToGet").append(`${data[i].foodProduct} <button class="addToPantry" data-id=${data[i].id}> Add To Pantry </button> <br>`);
     }
-    else{
+    else {
       var today = moment().format();
-      if(today >= data[i].expirationDate) {
+      if (today >= data[i].expirationDate) {
         $("#groceriesExpired").append(`${data[i].foodProduct} <br>`);
-      }else if(today >= data[i].expirationNotification) {
+      } else if (today >= data[i].expirationNotification) {
         $("#groceriesExpiringSoon").append(`${data[i].foodProduct} <br>`);
-      }else{
+      } else {
         $("#groceriesOwned").append(`${data[i].foodProduct} <br>`);
       }
     }
@@ -18,36 +18,39 @@ $.get("/members/getgroceries2").then(function (data) {
 });
 
 
-// console.log(moment().format())
+// This file just does a GET request to figure out which user is logged in
+// and updates the HTML on the page
+$.get("/api/user_data").then(function (data) {
+  $(".member-name").text(data.email);
+});
 
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/user_data").then(function (data) {
-    $(".member-name").text(data.email);
-  });
-  
-  
-  // this will update an item to be in the pantry and update DOM
-  $(document).on('click', '.addToPantry', function(){ 
-    $.ajax("/members/updategroceries", {
-      type: "PUT",
-      data: {id: $(this).data("id")}
-    }).then(
-      function () {
-        $.get("/members/getgroceries2").then(function (data) {
-          $("#groceriesToGet").empty();
-          $("#groceriesOwned").empty();
-          for (var i = 0; i < data.length; i++) {
-            if(!data[i].ownedItem){
-              $("#groceriesToGet").append(`${data[i].foodProduct} <button class="addToPantry" data-id=${data[i].id}> Add To Pantry </button> <br>`);
-            }
-            else{
+// this will update an item to be in the pantry and update DOM
+$(document).on('click', '.addToPantry', function () {
+  $.ajax("/api/updategroceries", {
+    type: "PUT",
+    data: { id: $(this).data("id") }
+  }).then(
+    function () {
+      $.get("/api/getgroceries").then(function (data) {
+        $("#groceriesToGet").empty();
+        $("#groceriesOwned").empty();
+        for (var i = 0; i < data.length; i++) {
+          if (!data[i].ownedItem) {
+            $("#groceriesToGet").append(`${data[i].foodProduct} <button class="addToPantry" data-id=${data[i].id}> Add To Pantry </button> <br>`);
+          }
+          else {
+            var today = moment().format();
+            if (today >= data[i].expirationDate) {
+              $("#groceriesExpired").append(`${data[i].foodProduct} <br>`);
+            } else if (today >= data[i].expirationNotification) {
+              $("#groceriesExpiringSoon").append(`${data[i].foodProduct} <br>`);
+            } else {
               $("#groceriesOwned").append(`${data[i].foodProduct} <br>`);
             }
           }
-        });
-        
+        }
       });
     });
-    
+});
+
 
