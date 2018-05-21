63c21945cadee2bda72eefffc6ed3f1afe945958
Ozair Khan <ozairsaddress@gmail.com>
2018-05-21 02:43:58 -0600

    Add to Panty Modal and update routes working

--- public/js/members.js
+++ public/js/members.js
@@ -1,28 +1,33 @@
-// get grocery items from databse, and sorts them in shlpping list or pantry
-$.get("/api/getgroceries").then(function (data) {
-  for (var i = 0; i < data.length; i++) {
-    //put in shopping list
-    if (!data[i].ownedItem) {
-      $("#groceriesToGet").append(`${data[i].foodProduct} <button class="addToPantry" data-id=${data[i].id}> Add To Pantry </button>
+// get grocery items from databse, and sorts them in shlpping list, pantry, expiring soon, or expired
+
+function renderPage() {
+  $.get("/api/getgroceries").then(function (data) {
+    for (var i = 0; i < data.length; i++) {
+      //put in shopping list
+      if (!data[i].ownedItem) {
+        $("#groceriesToGet").append(`${data[i].foodProduct} <button class="addToPantry" data-id=${data[i].id}> Add To Pantry </button>
       <button class="deleteItem" data-id=${data[i].id}>Delete </button> <br>`);
-    }
-    else {
-      var today = moment().format();
-      //put in expired list
-      if (today >= data[i].expirationDate) {
-        $("#groceriesExpired").append(`${data[i].foodProduct}<button class="deleteItem" data-id=${data[i].id}>Delete </button> <br>`);
       }
-      // put in expiring soon list 
-      else if (today >= data[i].expirationNotification) {
-        $("#groceriesExpiringSoon").append(`${data[i].foodProduct} <button class="deleteItem" data-id=${data[i].id}>Delete </button><br>`);
-      }
-      // put in pantry list
       else {
-        $("#groceriesOwned").append(`${data[i].foodProduct}<button class="deleteItem" data-id=${data[i].id}>Delete </button> <br>`);
+        var today = moment().format();
+        //put in expired list
+        if (today >= data[i].expirationDate) {
+          $("#groceriesExpired").append(`${data[i].foodProduct}<button class="deleteItem" data-id=${data[i].id}>Delete </button> <br>`);
+        }
+        // put in expiring soon list 
+        else if (today >= data[i].expirationNotification) {
+          $("#groceriesExpiringSoon").append(`${data[i].foodProduct} <button class="deleteItem" data-id=${data[i].id}>Delete </button><br>`);
+        }
+        // put in pantry list
+        else {
+          $("#groceriesOwned").append(`${data[i].foodProduct}<button class="deleteItem" data-id=${data[i].id}>Delete </button> <br>`);
+        }
       }
     }
-  }
-});
+  });
+}
+
+renderPage();
 
 
 // This file just does a GET request to figure out which user is logged in
@@ -33,61 +38,48 @@ $.get("/api/user_data").then(function (data) {
 
 // this will update an item to be in the pantry and update DOM
 $(document).on('click', '.addToPantry', function () {
-  $.ajax("/api/updategroceries", {
+  $("#updateModal").modal();
+  $("#itemId").val($(this).data("id"));
+  $(".hide2").css("display", "none");
+})
+
+$("#updateButton").on("click", function (event) {
+  // event.preventDefault();
+  // console.log( $("#updateExpiration").val(), $("#updateNotification").val() );
+  $.ajax("/api/updategroceries2", {
     type: "PUT",
-    data: { id: $(this).data("id") }
-  }).then(
-    function () {
-      $.get("/api/getgroceries").then(function (data) {
-        $("#groceriesToGet").empty();
-        $("#groceriesOwned").empty();
-        for (var i = 0; i < data.length; i++) {
-          //put in shopping list
-          if (!data[i].ownedItem) {
-            $("#groceriesToGet").append(`${data[i].foodProduct} <button class="addToPantry" data-id=${data[i].id}> Add To Pantry </button>
-            <button class="deleteItem" data-id=${data[i].id}>Delete </button> <br>`);
-          }
-          else {
-            var today = moment().format();
-            //put in expired list
-            if (today >= data[i].expirationDate) {
-              $("#groceriesExpired").append(`${data[i].foodProduct} <button class="deleteItem" data-id=${data[i].id}>Delete </button> <br>`);
-            }
-            // put in expiring soon list 
-            else if (today >= data[i].expirationNotification) {
-              $("#groceriesExpiringSoon").append(`${data[i].foodProduct} <button class="deleteItem" data-id=${data[i].id}>Delete </button><br>`);
-            }
-            // put in pantry list
-            else {
-              $("#groceriesOwned").append(`${data[i].foodProduct}<button class="deleteItem" data-id=${data[i].id}>Delete </button> <br>`);
-            }
-          }
-        }
-      });
-    });
-});
+    data: {
+      id: $("#itemId").val(),
+      updatedExpiration: $("#updateExpiration").val(),
+      updatedNotification: $("#updateNotification").val()
+    }
+  }).then(function () {
+
+  })
+})
+
 
 $(document).on('click', '.deleteItem', function () {
   $.ajax("/api/delete_item", {
     type: "POST",
     data: { id: $(this).data("id") }
   }).then(
-    function(){
-    location.reload();
+    function () {
+      location.reload();
     })
 });
 
 
 
-$("#owned").change(function(event) {
- console.log($(this).val())
-  if($(this).val() === "true"){
+$("#owned").change(function (event) {
+  console.log($(this).val())
+  if ($(this).val() === "true") {
     $(".hide").css("display", "inline-block");
-  } else{
+  } else {
     $(".hide").css("display", "none");
   }
 
-});         
+});
 
 
 
